45af5be6392b15a400e7b25d620bdef958496ac2
hrzolix <83654238+hrzolix@users.noreply.github.com>
2021-09-19 18:49:51 +0200

    removed

--- IMDB/imdb.py
+++ IMDB/imdb.py
@@ -4,11 +4,6 @@ url = "https://movie-database-imdb-alternative.p.rapidapi.com/"
 
 querystring = {"s":"Avengers Endgame","r":"json","page":"1"}
 
-headers = {
-    'x-rapidapi-host': "movie-database-imdb-alternative.p.rapidapi.com",
-    'x-rapidapi-key': "b2ad99815bmshabdd84715b31552p194ccdjsnf1751e22dfbe"
-    }
-
 response = requests.request("GET", url, headers=headers, params=querystring)
 
 print(response.text)
